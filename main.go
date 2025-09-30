dca82c9c9268d94dd6fd323bc6bcae3f23732b11
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-09-29 19:11:52 -0500

    Add initial project structure with database connection and routing setup

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/JuanLopezAranzazu/go-restapi-authentication-jwt/db"
+	"github.com/JuanLopezAranzazu/go-restapi-authentication-jwt/routes"
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hola Mundo")
+	// conexion con la base de datos
+	db.DBConnection()
+
+	// manejar rutas
+	r := mux.NewRouter()
+	// index
+	r.HandleFunc("/", routes.HomeHandler)
+
+	// iniciar servidor
+	log.Println("Servidor iniciado en http://localhost:3000")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal("Error al iniciar el servidor: ", err)
+	}
 }
